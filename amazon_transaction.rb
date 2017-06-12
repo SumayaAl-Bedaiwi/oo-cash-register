fbe8d16bbc458978aeee5c7bcb99e1d885d2adf1
Sumaya AlBedaiwi <queensumaya@gmail.com>
2017-06-12 13:48:38 +0000

    work in progress of amazon checkout lab

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,21 @@
 # Code your cash register here!
+
+class AmazonTransaction
+    attr_accessor :total
+    
+    def initialize(input = 0)
+        
+        @total = 0
+        @items = []
+        @discount = input
+    end
+    
+    def add_item(title, price, quantity = 1)
+        @total = @total + price*quantity
+        @total
+        
+        @items.push(title)
+        
+    end
+end
+
